91dbc7e3a94f66e7ce14c6c84f94bee6956e94e4
Lily <lilypadnebula@gmail.com>
2019-02-23 13:49:16 -0500

    Add console component

---
+++ src/components/ConsoleLine.vue
@@ -0,0 +1,10 @@
+<template>
+  <p class="text-left px-4">test@home: {{display}}<slot></slot></p>
+  
+</template>
+
+<script>
+export default {
+  props: ['display']
+}
+</script>
---
+++ src/views/Console.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h-most rounded bg-black text-white container mx-auto">
+    <div class="w-full p-4 text-center border-b mb-4">Test Console</div>
+    <console-line v-for="line in history" :key="line.id" :display="line.text"></console-line>
+    <console-line><input type="text" class="text-white bg-black focus:outline-none w-full" @keyup.enter="send"></console-line>
+  </div>
+</template>
+
+<script>
+import ConsoleLine from '../components/ConsoleLine.vue'
+
+export default {
+  components: {ConsoleLine},
+  data:() => ({
+    history: []
+  }),
+  methods: {
+    send(e){
+      this.history.push({id:Math.floor(Math.random()*Math.random()*1000), text:e.target.value})
+    }
+  }
+}
+</script>
+
+
+<style lang="postcss" scoped>
+.h-most {
+  height: 80vh;
+}
+</style>
